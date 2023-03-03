2ed7cb4c36fcfe1e947d19ca08e0dfd2bd1ccf95
Tufail Mehraj <shahtuffey@gmail.com>
2023-03-03 21:35:04 +0530

    first pushh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Get Set and Go!
+          Get SSSSet and Go!
         </p>
         <a
           className="App-link"
